bdfff6bbf70f9294af9027ace8ef04d0f5c6c717
Daniel Carpenter <dfcarpenter@gmail.com>
2024-03-20 15:55:08 -0700

    finishing main up for create and delete

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,126 @@
-mod crd;
+use std::sync::Arc;
+
+use futures::stream::StreamExt;
+use kube::runtime::watcher::Config;
+use kube::Resource;
+use kube::ResourceExt;
+use kube::{client::Client, runtime::controller::Action, runtime::Controller, Api};
+use tokio::time::Duration;
+
+use crate::crd::Echo;
+
+pub mod crd;
 mod echo;
 mod finalizer;
 
-fn main() {
-    println!("Hello, world!");
+struct ContextData {
+    client: Client,
+}
+
+impl ContextData {
+    pub fn new(client: Client) -> Self {
+        ContextData { client }
+    }
+}
+
+enum EchoAction {
+    Create,
+    Delete,
+    NoOp,
+}
+
+async fn reconcile(echo: Arc<Echo>, context: Arc<ContextData>) -> Result<Action, Error> {
+    let client: Client = context.client.clone();
+
+    // The resource of `Echo` kind is required to have a namespace set. However, it is not guaranteed
+    // the resource will have a `namespace` set. Therefore, the `namespace` field on object's metadata
+    // is optional and Rust forces the programmer to check for it's existence first.
+
+    let namespace: String = match echo.namespace() {
+        None => {
+            // If there is no namespace to deploy to defined, reconciliation ends with an error immediately.
+            return Err(Error::UserInputError(
+                "Expected Echo resource to be namespaced. Can't deploy to an unknown namespace."
+                    .to_owned(),
+            ));
+        }
+        // If namespace is known, proceed. In a more advanced version of the operator, perhaps
+        // the namespace could be checked for existence first.
+        Some(namespace) => namespace,
+    };
+    let name = echo.name_any(); // Name of the Echo resource is used to name the subresources as well.
+
+    match determine_action(&echo) {
+        EchoAction::Create => {
+            // Creates a deployment with `n` Echo service pods, but applies a finalizer first.
+            // Finalizer is applied first, as the operator might be shut down and restarted
+            // at any time, leaving subresources in intermediate state. This prevents leaks on
+            // the `Echo` resource deletion.
+
+            // Apply the finalizer first. If that fails, the `?` operator invokes automatic conversion
+            // of `kube::Error` to the `Error` defined in this crate.
+            finalizer::add(client.clone(), &name, &namespace).await?;
+            echo::deploy(client, &name, echo.spec.replicas, &namespace).await?;
+            Ok(Action::requeue(Duration::from_secs(10)))
+        }
+        EchoAction::Delete => {
+            // Deletes any subresources related to this `Echo` resources. If and only if all subresources
+            // are deleted, the finalizer is removed and Kubernetes is free to remove the `Echo` resource.
+
+            //First, delete the deployment. If there is any error deleting the deployment, it is
+            // automatically converted into `Error` defined in this crate and the reconciliation is ended
+            // with that error.
+            // Note: A more advanced implementation would check for the Deployment's existence.
+            echo::delete(client.clone(), &name, &namespace).await?;
+
+            // Once the deployment is successfully removed, remove the finalizer to make it possible
+            // for Kubernetes to delete the `Echo` resource.
+            finalizer::delete(client, &name, &namespace).await?;
+
+            Ok(Action::await_change()) // Makes no sense to delete after a successful delete, as the resource is gone
+        }
+        // The resource is already in desired state, do nothing and re-check after 10 seconds
+        EchoAction::NoOp => Ok(Action::requeue(Duration::from_secs(10))),
+    }
+}
+#[tokio::main]
+async fn main() {
+    let kubernetes_client: Client = Client::try_default().await.expect("Expected a valid KUBECONFIG variable");
+
+    let crd_api: Api<Echo> = Api::all(kubernetes_client.clone());
+
+    let context: Arc<ContextData> = Arc::new(ContextData::new(kubernetes_client.clone()));
+}
+
+fn determine_action(echo: &Echo) -> EchoAction {
+    if echo.meta().deletion_timestamp.is_some() {
+        return EchoAction::Delete;
+    } else if echo
+        .meta()
+        .finalizers
+        .as_ref()
+        .map_or(true, |finalizers| finalizers.is_empty())
+    {
+        EchoAction::Create
+    } else {
+        EchoAction::NoOp
+    }
+}
+
+
+fn on_error(echo: Arc<Echo>, error: &Error, _context: Arc<ContextData>) -> Action {
+    eprintln!("Reconciliation error:\n{:?}.\n{:?}", error, echo);
+    Action::requeue(Duration::from_secs(5))
+}
+
+/// All errors possible to occur during reconciliation
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Kubernetes reported error: {source}")]
+    KubeError {
+        #[from]
+        source: kube::Error,
+    },
+    #[error("Invalid Echo CRD: {0}")]
+    UserInputError(String),
 }
